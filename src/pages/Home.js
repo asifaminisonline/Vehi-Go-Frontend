51ac6435cb3a9e9eb519049f895b6f00dea5e5c5
Bludidi <bludidi5@gmail.com>
2023-06-01 10:48:28 +0200

    Update bootstrap version

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
-import React, { useEffect } from 'react';
+import React, { useRef, useEffect } from 'react';
+import { GrNext, GrPrevious } from 'react-icons/gr';
 import { useSelector, useDispatch } from 'react-redux';
 import { getCars } from '../redux/CarSlice';
 
@@ -10,28 +11,56 @@ const Homepage = () => {
     dispatch(getCars());
   }, [dispatch]);
 
+  const userefTarget = useRef();
+
+  const next = () => {
+    userefTarget.current.scrollLeft += 240;
+  };
+
+  const prev = () => {
+    userefTarget.current.scrollLeft -= 240;
+  };
+
   return (
     <div>
-      {
-        cars.loading ? <div className="flex justify-center min-h-[60vh] items-center">Car List</div>
-          : (
-            <div className="">
-              <div className="flex flex-col justify-center items-center mb-5">
-                <h1 className="md:text-2xl font-sans text-base font-bold text-green-600">Cars</h1>
-                <hr className="md:w-28 w-16 md:border-2 border-1 border-green-600" />
-              </div>
-              <div className="w-full h-54 items-center relative">
-                <button type="button" className="bg-green-400 hover:bg-green-500 text-lg p-1 absolute z-40 -right-7 top-28">Previous</button>
-                <button type="button" className="bg-green-400 hover:bg-green-500 text-lg p-1 absolute z-40 -right-7 top-28">Next</button>
-              </div>
-              <div ref className="flex items-center mx-2 md:scale-90 mt-2 gap-4 overflow-scroll scrollbar-none scroll-smooth scrollbar-hide transition-all">
-                {/* {cars.data[0]
+      {cars.loading ? (
+        <div className="flex justify-center min-h-[60vh] items-center">
+          Car List
+        </div>
+      ) : (
+        <div className="">
+          <div className="flex flex-col justify-center items-center mb-5">
+            <h1 className="md:text-2xl font-sans text-base font-bold text-green-600">
+              Cars
+            </h1>
+            <hr className="md:w-28 w-16 md:border-2 border-1 border-green-600" />
+          </div>
+          <div className="w-full h-54 items-center relative">
+            <button
+              type="button"
+              className="bg-green-400 hover:bg-green-500 text-lg p-1 absolute z-40 -right-7 top-28"
+              onClick={prev}
+            >
+              <GrPrevious />
+            </button>
+            <button
+              type="button"
+              className="bg-green-400 hover:bg-green-500 text-lg p-1 absolute z-40 -right-7 top-28"
+              onClick={next}
+            >
+              <GrNext />
+            </button>
+          </div>
+          <div
+            ref={userefTarget}
+            className="flex items-center mx-2 md:scale-90 mt-2 gap-4 overflow-scroll scrollbar-none scroll-smooth scrollbar-hide transition-all"
+          >
+            {/* {cars.data[0]
           && cars.data.map((elem) => (??))
                   } */}
-              </div>
-            </div>
-          )
-      }
+          </div>
+        </div>
+      )}
     </div>
   );
 };
