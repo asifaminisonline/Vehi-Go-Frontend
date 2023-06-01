674eb13f067bc9699b74ddb7797280f7e3614631
Bludidi <bludidi5@gmail.com>
2023-06-01 11:39:04 +0200

    Create car card

---
+++ src/components/EachCar.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const EachCar = ({
+  name, color, price, image, id,
+}) => (
+  <div className='flex flex-col justify-center min-h-[260px] min-w-[220px] p-1 bg-white border border-gray-200 drop-shadow-full hover:shadow-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 scale-100'>
+    <Link to={`/cars/${id}`}>
+      <div className="h-48 w-[13rem] mb-6 flex justify-center">
+        <img src={image} alt="" className="w-full h-full" />
+      </div>
+      <p className="text-green-600 font-bold text-lg capitalize">{name}</p>
+      <p className="text-slate-600 font-medium text-sm">${price}</p>
+      <p className="text-green-700 font-bold text-xs">
+        <span className="text-black">
+          {' '}
+          {color}
+        </span>
+      </p>
+    </Link>
+  </div>
+);
+
+export default EachCar;
