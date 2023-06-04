a6fb64a1c03e052ca179a3733c91e4dda1c1b334
tarikbouari <tarikbouari@gmail.com>
2023-06-04 12:18:29 +0000

    Add register reducer to the store

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import loginReducer from '../redux/LoginSlice';
+import loginReducer from '../redux/loginSlice';
+import registerReducer from '../redux/RegistrationSlice';
 
 export default configureStore({
   reducer: {
+    register: registerReducer,
     login: loginReducer,
   },
   // middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
