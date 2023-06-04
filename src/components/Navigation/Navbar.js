f30021e2ff703a23d17611c989489ff03db8f656
Bludidi <bludidi5@gmail.com>
2023-06-04 15:56:18 +0200

    Create navbar js file

---
+++ src/components/Navigation/Navbar.js
@@ -0,0 +1,56 @@
+/* eslint-disable */
+import React, { useState } from 'react';
+import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
+import { IconContext } from 'react-icons';
+import { Link } from 'react-router-dom';
+import { SidebarData } from './SidebarData';
+import './Navbar.css';
+
+function Navbar() {
+  const [sidebar, setSidebar] = useState(false);
+  const showSidebar = () => setSidebar(!sidebar);
+
+  return (
+    <>
+      <IconContext.Provider value={{ color: '#fff' }}>
+        <div
+          className="navbar"
+          style={{
+            backgroundColor: '#060b26',
+            height: '80px',
+            display: 'flex',
+            justifyContent: 'start',
+            alignItems: 'center',
+            paddingLeft: '10px',
+          }}
+        >
+          <Link to="/api/v1/cars" className="menu-bars">
+            <FaIcons.FaBars onClick={showSidebar} />
+          </Link>
+        </div>
+        <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+          <ul className="nav-menu-items" onClick={showSidebar}>
+            <li className="navbar-toggle">
+              <Link to="/api/v1/cars" className="menu-bars">
+                <AiIcons.AiOutlineClose />
+              </Link>
+            </li>
+            {SidebarData.map((item, index) => {
+              return (
+                <li key={index} className={item.cName}>
+                  <Link to={item.path}>
+                    {item.icon}
+                    <span>{item.title}</span>
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </IconContext.Provider>
+    </>
+  );
+}
+
+export default Navbar;
