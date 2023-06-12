825c58dd81e2f2429fba5f9d2f160bb2058f9415
tarikbouari <tarikbouari@gmail.com>
2023-06-12 12:14:02 +0000

    Update banner

---
+++ src/components/banner.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Banner = () => (
+
+  <div className="banner flex items-center ">
+
+    <div className="img-ca px-3">
+      <h1 className="mb-3 uppercase"> Rent your car by an easiest way </h1>
+      <h4 className="w-80 mb-3 font-light text-lg">100 luxury cars from multi brand are available for you</h4>
+      <button type="submit" className="rounded-full bg-orange px-3 py-2">Reserve</button>
+    </div>
+
+    <div className="img-banner" />
+
+  </div>
+);
+
+export default Banner;
