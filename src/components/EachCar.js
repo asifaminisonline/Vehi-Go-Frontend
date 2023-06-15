0588048b1d6d406bf860d3467cf1cb64744dc07e
tarikbouari <tarikbouari@gmail.com>
2023-06-15 01:48:29 +0000

    Style car page

--- src/components/EachCar.js
+++ src/components/EachCar.js
@@ -1,21 +1,39 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React, { useEffect } from 'react';
+import { toast } from 'react-hot-toast';
 import { useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { getCar } from '../redux/SingleCarSlice';
+import { addFavorite } from '../redux/favoriteSlice';
 import Loading from './Loading';
 
 const CarDetails = () => {
   const { id } = useParams();
   console.log(id);
+  const newDate = new Date().toISOString().split('T')[0];
+  console.log(newDate);
+  const user = localStorage.getItem('userId');
   const dispatch = useDispatch();
   // const { loading, car } = useSelector((state) => state.singleCar);
   const car = useSelector((state) => state.singleCar.car);
   const loading = useSelector((state) => state.singleCar.loading);
-
+  const fData = {
+    car_id: id,
+    user_id: user,
+    date: newDate,
+  };
+  const resData = {
+    reservation: fData,
+  };
   useEffect(() => {
     dispatch(getCar(id));
   }, [dispatch, id]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(addFavorite(resData));
+    toast.success('reserved successfully');
+  };
 
   if (loading) {
     return <div className="flex justify-center min-h-[60vh] items-center"><Loading /></div>;
@@ -39,7 +57,7 @@ const CarDetails = () => {
       }}
     >
       <div
-        className="p-4 align-items-center shadow-lg justify-content-center"
+        className=" ctn-show p-4 align-items-center shadow-lg justify-content-center"
         style={{
 
           backgroundColor: '#F7F5FA',
@@ -51,7 +69,7 @@ const CarDetails = () => {
       >
 
         <div
-          className="d-flex ms-3 mt-4"
+          className="d-flex showcar ms-3 mt-4"
           style={{ maxWidth: '1440px' }}
         >
           <div>
@@ -66,7 +84,7 @@ const CarDetails = () => {
               }}
             />
           </div>
-          <div className="px-4" style={{ maxWidth: '600px' }}>
+          <div className=" ctn-words" style={{ maxWidth: '600px' }}>
             <h4 className=" uppercase">{car.name}</h4>
             <p
               className="text-start"
@@ -79,9 +97,9 @@ const CarDetails = () => {
               {car.description}
             </p>
             {/* <div className="bg-darkblue h-1 mx-2 mb-2" /> */}
-            <div className="flex items-center justify-between">
+            <div className="flex items-center justify-between gap-2">
 
-              <span className="font-medium">
+              <span className="font-medium mx-0">
                 Price: &nbsp;
                 <b className="mx-0">
                   $
@@ -93,6 +111,7 @@ const CarDetails = () => {
                 type="button"
                 className=" rounded-full bg-orange  hover:bg-orangelt px-3 py-1 text-white"
                 style={{ display: 'inline-block', width: 'fit-content' }}
+                onClick={handleSubmit}
               >
                 Favourite
               </button>
